c52cb48eab9ff46643b28cce9a31d96355d9ea23
Houssame Zerouali <azenax328@gmail.com>
2021-08-02 12:35:53 +0200

    finished the forms to login

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const authRouter = require('./routes/auth');
 const app = express();
 
 // view engine setup
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
