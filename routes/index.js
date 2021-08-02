ede36c9cdd26a07dacd3ef02a4fbb907d946c19f
Chenjia Li <cligrenet@gmail.com>
2021-08-02 12:20:28 +0200

    added some routes

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,28 @@ const User = require('../models/User.model');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('home.hbs', {});
+router.get('/', (req, res, next) => {
+  res.render('home.hbs', {
+    style: 'stylesheets/style.css',
+  });
+});
+
+//GET Recipes List page//
+router.get('/recipes', (req, res, next) => {
+  Recipe.find().then((dbRes) => {
+    res.render('/recipes.hbs', {
+      recipes: dbRes,
+    });
+  });
+});
+
+//GET One Recipe Page//
+router.get('/recipes/:id', (req, res, next) => {
+  Recipe.findById(req.params.id).then((dbRes) => {
+    res.render('oneRecipe.hbs', {
+      Recipe: dbRes,
+    });
+  });
 });
 
 module.exports = router;
